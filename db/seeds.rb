36587820fb4f1ecfdc57dca661f212b61d554d41
Jalynn Ridley <jallywally@gmail.com>
2021-05-25 14:54:58 +0200

    made route for show one game

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# t.string "name"
+#     t.string "description"
+#     t.string "type"
+#     t.integer "min_players"
+#     t.integer "max_players"
+#     t.boolean "rented"
+#     t.integer "price"
+
