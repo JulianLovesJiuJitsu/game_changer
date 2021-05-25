a9cd36b4144b3256522b56fecefef8454ca307cd
Julian Thompson <j.thompson@hotmail.de>
2021-05-25 16:02:33 +0200

    updated the view for new and added basic validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,12 @@ class Game < ApplicationRecord
   belongs_to :user
 
   has_many :users, through: :rentals
+
+  validates :name, :description, :category, presence: true
+
+  validates_numericality_of :min_players, :max_players, :greater_than_or_equal_to => 1, :only_integer => true
+
+  validates :price, numericality: true
+
+  validates :category, inclusion: { in: %w(Strategy Cards Kids Economy RPG Cooperative) }
 end
